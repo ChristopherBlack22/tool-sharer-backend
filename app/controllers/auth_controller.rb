947643569431b705e653df723407f0e37669c320
Christopher <christopher.black.2209@googlemail.com>
2022-02-12 01:13:13 +0000

    updated routes

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,6 @@
 class AuthController < ApplicationController
+
+    def create
+
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :tools
-  resources :users
+  #resources :categories
+  #resources :tools, only: [:index, :create, :update]
+  resources :users, only: [:create]
+  post "login", to: "auth#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
