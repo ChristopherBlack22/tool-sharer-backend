24017035db662d223801604f51686ff0f8435fbc
Christopher <christopher.black.2209@googlemail.com>
2022-02-20 02:40:49 +0000

    Tools index route and action added

--- app/controllers/api/v1/tools_controller.rb
+++ app/controllers/api/v1/tools_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::ToolsController < ApplicationController
+    
+    def index
+        tools = Tool.all
+        render json: ToolSerializer.new(tools)
+        #improve serialized data
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      #resources :categories
-      #resources :tools, only: [:index, :create, :update]
-      #resources :users, only: [:create]
+      #resources :categories, only: [:index]
+      resources :tools, only: [:index, :create, :update]
       post "/signup", to: "users#create"
       post "/login", to: "auth#create"
 
